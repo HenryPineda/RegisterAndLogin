63d4b1dec2b29b799b65563e542c08f1392ea87a
HenryPineda <pinedahenryandres@gmail.com>
2019-07-17 17:42:45 -0600

    First commit creating the page template and creating the database in part 0 and part 1

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Register and login course</title>
+</head>
+<body>
+
+    <header>
+
+        <h1 class="logo">Logo</h1>
+
+        <nav>
+
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="downloads.php">Downloads</a></li>
+                <li><a href="forum.php">Forum</a></li>
+                <li><a href="contact.php">Contact us</a></li>
+            
+            </ul>
+        
+        </nav>
+
+        <div class="clear"></div>
+
+    </header>
+
+    <div id="container">
+
+        <aside>
+
+            <div class="widget">
+                <h2>Widget Header</h2>
+                <div class="inner">
+
+                    Widget contents
+                
+                </div>
+            
+            </div>
+        </aside>
+
+        <h1>Home</h1>
+        <p>Just a template</p>
+    
+    </div>
+
+    <footer>
+
+        &copy; phpacademey.org 2011. All rights reserved.
+
+    </footer>
+
+    
+</body>
+</html>
